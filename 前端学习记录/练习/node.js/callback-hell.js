18bb1e6cfab849b91969f5fe930273fdc443d90b
dedsec <1115659949@qq.com>
2022-09-30 14:06:25 +0800

    学习记录

--- 前端学习记录/练习/node.js/callback-hell.js
+++ 前端学习记录/练习/node.js/callback-hell.js
@@ -16,61 +16,74 @@ const fs = require('fs')
 
 // promise
 
-function myPlan (path) {
-  return new Promise((resolve,reject)=>{
-    fs.readFile(path,(err,data)=>{
-      if(err){
-        reject(err)
-      }
-      resolve(data)
-    })
-  })
-}
+// function myPlan (path) {
+//   return new Promise((resolve,reject)=>{
+//     fs.readFile(path,(err,data)=>{
+//       if(err){
+//         reject(err)
+//       }
+//       resolve(data)
+//     })
+//   })
+// }
 
 
-myPlan('./txt/1.txt').then(res => {
-  return myPlan(res)
-}, reason => {
-  console.log(reason);
-}).then(res=>{
-  fs.writeFile('./txt/3.txt',res.toString(),err=>{
-    if(err)return
-    console.log('写入成功');
-  })
-},reason=>{
-  console.log(reason);
-})
+// myPlan('./txt/1.txt').then(res => {
+//   return myPlan(res)
+// }, reason => {
+//   console.log(reason);
+// }).then(res=>{
+//   fs.writeFile('./txt/3.txt',res.toString(),err=>{
+//     if(err)return
+//     console.log('写入成功');
+//   })
+// },reason=>{
+//   console.log(reason);
+// })
 
 
 // // await&async
 
-// async function myPlan2(path){
-//   let p=new Promise((resolve,reject)=>{
-//     fs.readFile(path,(err,data)=>{
-//       if(err)reject(err)
+// async function myPlan2 (path) {
+//   let p = new Promise((resolve, reject) => {
+//     fs.readFile(path, (err, data) => {
+//       if (err) reject(err)
 //       resolve(data)
 //     })
 //   })
 
-//   let f2=await p
-//   let res=new Promise((resolve,reject)=>{
-//     fs.readFile(f2,(err,data)=>{
-//       if(err)reject(err)
+//   let f2 = await p
+//   let res = new Promise((resolve, reject) => {
+//     fs.readFile(f2, (err, data) => {
+//       if (err) reject(err)
 //       resolve(data)
 //     })
 //   })
-//   let r2=await res 
+//   let r2 = await res
 
-//   let res3=new Promise((resolve,reject)=>{
-//     fs.writeFile('./txt/3.txt',r2,err=>{
-//       if(err)reject(err)
+//   let res3 = new Promise((resolve, reject) => {
+//     fs.writeFile('./txt/3.txt', r2, err => {
+//       if (err) reject(err)
 //       resolve('写入成功')
 //     })
 //   })
 
-//   let r3= await res3
+//   let r3 = await res3
 //   console.log(r3);
 //   return r3
 // }
 
 // myPlan2('./txt/1.txt')
+
+
+
+// promisify
+const util=require('util')
+
+let myReadFile=util.promisify(fs.readFile)
+
+myReadFile('./txt/1.txt').then(res=>{
+  console.log(res);
+},reason=>{
+  console.log(reason);
+})
