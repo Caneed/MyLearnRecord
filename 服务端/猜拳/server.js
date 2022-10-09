98391e9305e00254a883799068fa008830d1a91d
dedsec <1115659949@qq.com>
2022-10-09 10:23:35 +0800

    学习记录

--- 服务端/猜拳/server.js
+++ 服务端/猜拳/server.js
@@ -1,30 +1,8 @@
 
-const fs=require('fs')
+const xhr=new XMLHttpRequest()
 
-const http=require('http')
-const port=3000
-const {hostname}=require('os')
+xhr.open('GET','http://127.0.0.1:3000')
 
 
-const server=http.createServer()
 
-server.on('request',(req,res)=>{
-  fs.readFile('./index.html',(err,data)=>{
-    if(err)return
-    res.setHeader('Content-Type','text/html;charset=utf-8')
-    res.end(data)
-  })
 
-  console.log(req.url);
-  if(req.url.includes('.js')){
-    console.log('js');
-    fs.readFile(req.url,(err,data)=>{
-      if(err)return
-      res.setHeader('Content-Type','text/js;charset=utf-8')
-    })
-  }
-})
-
-server.listen(port,()=>{
-  console.log(`服务器在${hostname}:${port}上运行`);
-})
