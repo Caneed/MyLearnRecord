69bf1d7f8e4916de6a156a7e9e905d69732d9c5d
dedsec <1115659949@qq.com>
2022-10-10 17:21:26 +0800

    学习记录

---
+++ 前端学习记录/AJAX/post.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use('/public', express.static(`${__dirname}`))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/xhr_get', (req, res) => {res.send('AJAX get')})
+app.post('/xhr_post', (req, res) => {res.send('AJAX post')})
+app.post('/axios_get', (req, res) => {res.send('axios get')})
+app.post('/axios_post', (req, res) => {res.send('axios post')})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
