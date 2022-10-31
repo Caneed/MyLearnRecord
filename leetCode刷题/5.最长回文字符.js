72800c4c8ec73a250ef725735e26a15fd71e7951
dedsec <1115659949@qq.com>
2022-10-31 13:05:20 +0800

    学习记录

---
+++ leetCode刷题/5.最长回文字符.js
@@ -0,0 +1,17 @@
+// 给你一个字符串 s，找到 s 中最长的回文子串。
+// 输入：s = "babad"
+// 输出："bab"
+// 解释："aba" 同样是符合题意的答案。
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestPalindrome = function(s) {
+console.log(s.substring(3))
+};
+
+
+
+let s='hello'
+console.log(s.slice(1,3))
