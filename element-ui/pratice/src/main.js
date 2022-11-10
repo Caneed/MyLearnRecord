c18e3036805e836abf5bc0b8b5e4f0cf1f282ae1
dedsec <1115659949@qq.com>
2022-11-10 17:33:57 +0800

    学习记录

--- element-ui/pratice/src/main.js
+++ element-ui/pratice/src/main.js
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入element-ui
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.full'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+.use(store)
+.use(router)
+.use(ElementPlus,{
+  locale:zhCn
+})
+.mount('#app')
