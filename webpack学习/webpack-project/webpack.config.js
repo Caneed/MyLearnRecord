85fffb8d813dfba10525a0950038f9bdfacf2ba6
dedsec <1115659949@qq.com>
2022-11-15 15:17:49 +0800

    学习记录

--- webpack学习/webpack-project/webpack.config.js
+++ webpack学习/webpack-project/webpack.config.js
@@ -1,8 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  // JavaScript 执行入口文件
+  // JavaScript 执行入口文件 Entry
   entry: './asset/main.js',
+
+
+  //修改context就是修改默认根路径
+  // context:path.resolve(__dirname,'xxx'),
+
+  // 动态配置entry
+  // 同步配置entry
+  // entry:()=>{
+  //   return{
+  //     a:'./asset/a',
+  //     b:'./asset/b'
+  //   }
+  // },
+
+  // 异步配置：使用promise
+  // entry:()=>{
+  //   return new Promise(resolve=>{
+  //     resolve({a:'./asset/a',b:'./asset/b'})
+  //   })
+  // },
+
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
     filename: 'bundle.js',
