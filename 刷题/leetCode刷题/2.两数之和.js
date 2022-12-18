8bd536f0f90b9b186f781f8a0cf616cc7d07de76
dedsec <1115659949@qq.com>
2022-12-18 15:37:58 +0800

    学习记录

--- 刷题/leetCode刷题/2.两数之和.js
+++ 刷题/leetCode刷题/2.两数之和.js
@@ -11,24 +11,26 @@
  */
 
 // 1.暴力双循环
-var twoSum = function(nums, target) {
-  for(let i=0;i<nums.length;i++){
-      for(let j=i+1;j<nums.length;j++){
-          if(nums[i]+nums[j]===target)
-          return [i,j]
-      }
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target)
+        return [i, j]
+    }
   }
 };
 
-
-// 2.hashMap
-var twoSum = function(nums, target) {
-  const map = new Map();
-  for(let i = 0, len = nums.length;i < len;i++) {
-      if(map.has(target - nums[i])) {
-          return [map.get(target - nums[i]), i];
-      }
-      map.set(nums[i], i);
+// 2.map 做法
+var twoSum = function (nums, target) {
+  const map = new Map()
+  // 遍历
+  for (let i = 0, len = nums.length; i < len; i++) {
+    // 在map中加入数组的值和数组的下标
+    map.set(nums[i], i)
+    // 如果map中含有 总数减去当前循环中的数组值的插值
+    if (map.has(target - nums[i])) {
+      // 直接返回这两个下标
+      return [map.get(target - nums[i]), i]
+    }
   }
-  return [];
-};
+}
