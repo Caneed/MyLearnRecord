572d21105d0a2af1be4f882e16e1795b5a14da90
091668 <liumengckg@travelsky.com.cn>
2023-10-19 17:10:56 +0800

    learn

--- Dart/code/变量.dart
+++ Dart/code/变量.dart
@@ -3,4 +3,10 @@ void main() {
   String name2 = '小明';
   dynamic name3 = '小明';
   int a;
+  final age = 30;
+  final String name4 = '小红';
+  const num = 300000;
+  const double number = 3.14159265;
+  var foo=const [];
+  final fee=const [];
 }
