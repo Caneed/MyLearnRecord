85edae4dc96ae4bd01bf8a6151e8058040d278ca
091668 <liumengckg@travelsky.com.cn>
2023-10-23 13:58:24 +0800

    Flutter前置——Dart学习

---
+++ Dart/code/内建类型.dart
@@ -0,0 +1,14 @@
+void main() {
+  var a = 1.2345;
+  var b = 1;
+  var str1 = 'hello';
+  var str2 = "hola";
+  var muti = '''
+hahahaha
+hahahaa
+heihehihei
+''';
+  var row = r'row stri';
+  var arr = [1, 2, 3];
+  var constantArr = const [1, 2, 3];
+}
