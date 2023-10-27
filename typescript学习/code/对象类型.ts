3c4de776eef71269b9ec833d2a16ccbd1a34d772
091668 <liumengckg@travelsky.com.cn>
2023-10-27 16:02:15 +0800

    ts对象类型

--- typescript学习/code/对象类型.ts
+++ typescript学习/code/对象类型.ts
@@ -53,3 +53,90 @@
 // // { name: 'zs', age: 21 }
 
 // ---------------------------------------------------
+// declare function getStringArray(): StringArray
+// interface StringArray {
+//   [index: number]: string
+// }
+
+// interface NumArray {
+//   [index: string]: number
+// }
+
+// let arr: StringArray = []
+
+// // arr[0] = 1
+
+// arr[0] = '1'
+
+// let arrNum: NumArray = {
+//     a:1,
+//     b:2,
+//     d:4,
+//     // e:'3'//err
+// }
+
+// console.log(arrNum['a']) //1
+// -----------------------------------
+// interface TrafficTool {
+//   price: number
+//   way: 'fly' | 'run' | 'ride'
+// }
+
+// interface Car extends TrafficTool {
+//   action: 'beep' | 'turn'
+// }
+
+// const car:Car={
+//     price:200000,
+//     way:'run',
+//     action:'beep'
+// }
+// =====================================
+
+// interface Circle{
+//     radius:number
+// }
+
+// interface Colorful{
+//     color:string
+// }
+
+// type ColorfulCircle=Circle&Colorful
+
+// const colorfulCircle:ColorfulCircle={
+//     radius:20,
+//     color:'red'
+// }
+
+// -----------------------------------
+
+// interface MyType<T>{
+//     name:T
+// }
+
+// const tp:MyType<string> = {
+//     name:'aaa'
+// }
+
+// =====================================
+
+// function names(names: Array<string>) {
+//   console.log(names)
+// }
+
+// const arr=new Array()
+// const arr=new ReadOnlyArray//error
+
+// const arr:ReadonlyArray<number>=[1,2,3]
+// const arr:readonly number[]=[1,2,3]
+// let arr: readonly number[] = [1, 2, 3]
+// let arr1: number[] = [1, 2, 3]
+
+// arr = arr1
+// arr1 = arr //err:类型 "readonly number[]" 为 "readonly"，不能分配给可变类型 "number[]"。
+
+// --------------------------------------------------------------------
+
+// type Tp=[number,string]
+
+// const tp:Tp=[1,'1']
