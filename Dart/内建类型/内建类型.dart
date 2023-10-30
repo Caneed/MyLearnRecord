e13d459f84740448c4a8a7ee9fe3adbaf07e5711
091668 <liumengckg@travelsky.com.cn>
2023-10-30 17:26:12 +0800

    dart——文件整理

---
+++ Dart/内建类型/内建类型.dart
@@ -0,0 +1,28 @@
+void main() {
+//   var a = 1.2345;
+//   var b = 1;
+//   var str1 = 'hello';
+//   var str2 = "hola";
+//   var muti = '''
+// hahahaha
+// hahahaa
+// heihehihei
+// ''';
+//   var row = r'row stri';
+//   var arr = [1, 2, 3];
+//   var constantArr = const [1, 2, 3];
+//   var set = {'qdqwqw', 'dadafafa', 'dqdqqq'};
+//   var emptySet = <String>{};
+//   Set <String>eSet = {};
+
+  var map={
+    'name':'xm',
+    'age':'13',
+    'sex':'male'
+  };
+
+  var m=new Map();
+  m['name']='xm';
+  m['age']='12';
+  m['sex']='male';
+}
