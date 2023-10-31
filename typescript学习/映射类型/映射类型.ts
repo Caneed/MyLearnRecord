a0f7f843c75c4819dcc8d9225e8c682be03d5c26
091668 <liumengckg@travelsky.com.cn>
2023-10-31 10:04:47 +0800

    ts——映射类型

--- typescript学习/映射类型/映射类型.ts
+++ typescript学习/映射类型/映射类型.ts
@@ -1,8 +1,68 @@
-type BoolsAndHorse = {
-  [index: string]: boolean | Horse
-}
+// type Item = {
+//   x: string
+//   y: number
+// }
 
-const conforms = {
-  del: true,
-  rodney: false,
-}
+// // T1 = {a:number, b:number}
+// type T1 = {
+//   [P in 'a' | 'b']: number
+// }
+
+// // T2 = {a:'a', b:'b'}
+// type T2 = {
+//   [p in 'a' | 'b']: p
+// }
+
+// // T3 = {x:'x', y:'y'}
+// type T3 = {
+//   [P in keyof Item]: P
+// }
+
+// // type T4 = {
+// //   readonly x?: string | undefined;
+// //   readonly y?: number | undefined;
+// // }
+// type T4 = {
+//   +readonly [P in keyof Item]?: Item[P]
+// }
+
+// -----------------------------------------------------------------
+
+// type User = {
+//   name: string
+//   age: number
+//   gender: 'male' | 'female'
+// }
+
+// type MyPartial<T> = {
+//   +readonly [P in keyof T]?: T[P]
+// }
+
+// type PartialUser = MyPartial<User>
+
+// // type PartialUser = {
+// //   readonly name?: string | undefined;
+// //   readonly age?: number | undefined;
+// //   readonly gender?: "male" | "female" | undefined;
+// // }
+
+// ------------------------------------------------------------------
+
+// type User = {
+//   name:string
+//   age:number
+// }
+
+// type MyMapperPartial<Type>={
+//   [Properties in keyof Type as `mapper${Capitalize<string & Properties>}`]:Type[Properties]
+// }
+
+// type MapperPartialUser= MyMapperPartial<User>
+
+// // type MapperPartialUser = {
+// //   mapperName: string;
+// //   mapperAge: number;
+// // }
+
+
+// ----------------------------------------------------------------------------
