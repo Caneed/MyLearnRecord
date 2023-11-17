47010ff9006deb220f19127cc71c5c4665b028ec
091668 <liumengckg@travelsky.com.cn>
2023-11-17 13:16:56 +0800

    算法：leetcode

--- leetcode/动态规划/使用最小花费爬楼梯.ts
+++ leetcode/动态规划/使用最小花费爬楼梯.ts
@@ -24,15 +24,8 @@
 function minCostClimbingStairs(cost: number[]): number {
     let l = cost.length
     if (l == 2) return Math.min(cost[0], cost[1])
-    
+    for (let i = 2; i < l; i++) {
+        cost[i] = cost[i] + Math.min(cost[i - 1], cost[i - 2])
+    }
+    return Math.min(cost[l - 1], cost[l - 2])
 }
-// function minCostClimbingStairs(cost: number[]): number {
-//     // 记录长度
-//     let l = cost.length
-//     // 如果两阶，返回较小的一阶
-//     if (l === 2) return Math.min(cost[0], cost[1])
-//     for (let i = 2; i < l; i++) {
-//       cost[i] = cost[i] + Math.min(cost[i - 1], cost[i - 2])
-//     }
-//     return Math.min(cost[l - 1], cost[l - 2])
-// };
