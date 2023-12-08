da5ab2ffecf53d463f5afb32d90da4ae6eb85d02
091668 <liumengckg@travelsky.com.cn>
2023-12-08 11:16:24 +0800

    服务端渲染SSR:nuxt.js 后端学习：java集合

---
+++ nuxt-learn/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{hello}}
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const hello=ref
+</script>
+
+<style scoped>
+
+</style>
--- nuxt-learn/nuxt.config.ts
+++ nuxt-learn/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// 配置选项
 export default defineNuxtConfig({
   devtools: { enabled: true }
 })
