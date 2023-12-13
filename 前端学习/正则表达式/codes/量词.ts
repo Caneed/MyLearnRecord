96c7dd483c0633010183e3b948acee06103bba63
091668 <liumengckg@travelsky.com.cn>
2023-12-13 10:29:21 +0800

    正则表达式——量词

---
+++ 前端学习/正则表达式/codes/量词.ts
@@ -0,0 +1,69 @@
+/* -----------------------x*--------------------*/
+/* -----------------------x*--------------------*/
+
+// console.log(/bo*/.exec('boooooooooooooo'));
+// // [
+//     //     'boooooooooooooo',
+//     //     index: 0,
+//     //     input: 'boooooooooooooo',
+//     //     groups: undefined
+//     //   ]
+
+//     console.log(/bo*/.exec('bird'));
+//     // [ 'b', index: 0, input: 'bird', groups: undefined ]
+
+/* -----------------------x+--------------------*/
+
+// console.log(/bo+/.exec('boooooooooooo'));
+// // [
+// //     'boooooooooooo',
+// //     index: 0,
+// //     input: 'boooooooooooo',
+// //     groups: undefined
+// //   ]
+// console.log(/bo+/.exec('bird'));
+// // null
+
+
+
+/* -----------------------x?--------------------*/
+
+// console.log(/bo?/.exec('booooooooooooo'));
+// // [ 'bo', index: 0, input: 'booooooooooooo', groups: undefined ]
+// console.log(/bo?/.exec('bird'));
+// // [ 'b', index: 0, input: 'bird', groups: undefined ]
+
+/* -----------------------x{n}--------------------*/
+
+// console.log(/bo{2}/.exec('boooooooooo'));
+// // [ 'boo', index: 0, input: 'boooooooooo', groups: undefined ]
+// console.log(/bo{3}/.exec('booooooooo'));
+// // [ 'booo', index: 0, input: 'booooooooo', groups: undefined ]
+
+/* -----------------------x{n,}--------------------*/
+
+// console.log(/bo{3,}/.exec('booooooooooooo'));
+// // [
+// //     'booooooooooooo',
+// //     index: 0,
+// //     input: 'booooooooooooo',
+// //     groups: undefined
+// //   ]
+// console.log(/bo{3,}/.exec('boo'));
+// // null
+
+/* -----------------------x{n,m}--------------------*/
+
+// console.log(/bo{2,5}/.exec('booooooooooooooooooo'));
+// // [
+// //     'booooo',
+// //     index: 0,
+// //     input: 'booooooooooooooooooo',
+// //     groups: undefined
+// //   ]
+// console.log(/bo{2,5}/.exec('boo'));
+// // [ 'boo', index: 0, input: 'boo', groups: undefined ]
+// console.log(/bo{2,5}/.exec('booo'));
+// // [ 'booo', index: 0, input: 'booo', groups: undefined ]
+// console.log(/bo{2,5}/.exec('bo'));
+// //null
