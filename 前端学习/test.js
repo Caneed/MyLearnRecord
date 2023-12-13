e18f00c4d403af1c703ff87632cb55bcd44501d7
091668 <liumengckg@travelsky.com.cn>
2023-12-13 16:24:28 +0800

    整理

--- 前端学习/test.js
+++
@@ -1,9 +0,0 @@
-const a = 50
-
-console.log(a.toFixed(2))
-console.log(Number.parseFloat(a.toFixed(2)))
-console.log(Number.parseInt(a.toFixed(2)))
-console.log(~~a.toFixed(2))
-console.log(a.toFixed(2) + 0)
-console.log(Math.floor(a.toFixed(2)))
-console.log(a.toFixed(2) * 1.00)
